dcf4c81920cc1939b35da1b86d12d4bbef82266c
Matt Visnovsky <matt@visnovsky.us>
2022-10-11 14:40:21 -0600

    Disallow blank fields by default

--- htdocs/js/functions.js
+++ htdocs/js/functions.js
@@ -23,21 +23,12 @@ function EditAttributeValidate(attribute,selector){
             validated = selector.value.match(regex) ? true : false;
             message = "Input must be a 7-digit ID.";
             break;
-        case "displayname":
             validated = !isEmpty(selector.value) ? true : false;
-            message = "Field cannot be blank.";
-            break;
-        case "sn":
-            validated = !isEmpty(selector.value) ? true : false;
-            message = "Field cannot be blank.";
-            break;
-        case "givenname":
-            validated = !isEmpty(selector.value) ? true : false;
-            message = "Field cannot be blank.";
             break;
         default:
-            validated = true;
-            message = "";
+            validated = !isEmpty(selector.value) ? true : false;
+            message = "Field cannot be blank.";
+            break;
     }
 
     return { validated, message };
