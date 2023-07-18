15ca5c30bcc3de69180ada3d0b983a29d8059464
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-07-18 14:30:49 -0600

    Add Request Edit endpoint for POST requests
    This should handle all the backend work for sending emails, but
    currently it is just a placeholder

---
+++ requestedit.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!isset($_POST["attribute"]) or !isset($_POST["dn"]) or !isset($_POST["editField"]) or !$authenticated) {
+    $result = "Invalid State";
+    header('Location: index.php?page=display&editattributeresult='.$result);
+    return;
+}
+
+$dn = $_POST["dn"];
+$attribute = $_POST["attribute"];
+$edits = $_POST["editField"];
+
+// Send Email that says '{dn} is requesting a change to {attribute}: {edits}'
+
+$result = "Successfully requested change to ".$attribute;
+header('Location: index.php?page=display&editattributeresult='.$result);
